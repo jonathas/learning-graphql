42f007790b7c10d9384b4ce4dba6c2eb62c6d640
Jonathas Ribeiro <jonathas.ribeiro@cloudtalk.io>
2022-01-10 18:10:44 +0100

    feat: using optimisticResponse for setting the number of likes

--- Lyrical-GraphQL/client/components/LyricList.jsx
+++ Lyrical-GraphQL/client/components/LyricList.jsx
@@ -3,8 +3,20 @@ import { graphql } from 'react-apollo';
 import { likeLyric } from '../graphql/queries-and-mutations';
 
 class LyricList extends Component {
-  onLike(id) {
-    this.props.mutate({ variables: { id } });
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      // Updating the UI before the response comes back from the server.
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
+      // After the response comes back from the server, it overrides the optimistic response we set above.
+    });
   }
 
   renderLyrics() {
@@ -14,7 +26,7 @@ class LyricList extends Component {
         <div>
           <i
             className="material-icons"
-            onClick={() => this.onLike(id)}
+            onClick={() => this.onLike(id, likes)}
             role="button"
             tabIndex="-1"
             onKeyPress={() => {}}
